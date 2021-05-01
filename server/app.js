32fdaf9ca43f784cf5886b308572ceb66b657f95
Tommy Wei <tommywei@google.com>
2021-04-30 21:24:09 -0700

    fix rout for angular page

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '..', 'dist')))
+const angularDir = path.join(__dirname, '..', 'dist', 'my-app');
+
+app.use(express.static(angularDir));
 
 app.use('/api', require('./api'))
 
